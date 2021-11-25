0b833a9b688d5b4b4e49b3e2a033fe997886747c
alekseizelemskii <zelemskiialeksei@gmail.com>
2021-11-25 22:53:12 +0200

    fixed tests for sagas

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -4,6 +4,6 @@ import { Items } from '../reducers/itemsReducer';
 
 export const getData = (): AnyAction => ({type: ActionType.KNOCKING_TO_API });
 
-export const fetchSuccess = (payload: Items): AnyAction => ({type: ActionType.FETCH_SUCCESS, payload });
+export const fetchSuccess = (payload: Items[]): AnyAction => ({type: ActionType.FETCH_SUCCESS, payload });
 
 export const fetchFailure = (): AnyAction => ({type: ActionType.FETCH_FAILURE})
--- src/redux/sagas/index.ts
+++ src/redux/sagas/index.ts
@@ -1,32 +1,27 @@
 import { ActionType } from './../action-types/index';
 import {put, takeEvery, call} from 'redux-saga/effects';
 import { fetchSuccess, fetchFailure } from '../action-creators';
-import { expectSaga } from "redux-saga-test-plan";
-
 
 export const fetchItemsFromApi = async () => {
-    const data = await fetch(`http://myjson.dit.upm.es/api/bins/fbe1`)
-    const json = await data.json();
-    return json;
+    const response = await fetch(`http://myjson.dit.upm.es/api/bins/fbe1`)
+    const {products} = await response.json();
+    return products;
 };
 
-
 export function* fetchWorker(): any {
-
     try {
-        const {products} = yield call(fetchItemsFromApi);
-        
+        const products = yield call(fetchItemsFromApi);
         yield put(fetchSuccess(products)); 
 
     }catch (error) {
         yield put(fetchFailure())
     }
-}
+};
 
 export function* fetchWatcher() {
     yield takeEvery(ActionType.KNOCKING_TO_API, fetchWorker);
-}
+};
 
 export function* rootSaga(){
     yield fetchWatcher();    
-} 
+};
